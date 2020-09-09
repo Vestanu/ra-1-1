ab180228dd8a82e30f36ba801a0da24c917d1435
Tatiana Penkova <tatianapenkova@gmail.com>
2020-09-09 17:57:45 +0300

    2d37be2

---
+++ precache-manifest.8acd8e2ec182801a18ba88661d024bae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcff5c24144cb246caf2ed172e64d521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ecbdc45828b18d4d41a",
+    "url": "/static/css/main.12855e38.chunk.css"
+  },
+  {
+    "revision": "70b24ddc38734c185e28",
+    "url": "/static/js/2.3c7ecf5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c7ecf5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ecbdc45828b18d4d41a",
+    "url": "/static/js/main.b9efc491.chunk.js"
+  },
+  {
+    "revision": "955d07b3112ffa10e906",
+    "url": "/static/js/runtime-main.bb5b2b8f.js"
+  },
+  {
+    "revision": "9c63a2c92146aa97904b9148bc055ce3",
+    "url": "/static/media/item-black.9c63a2c9.jpg"
+  },
+  {
+    "revision": "8bcbdffead0d5ba6700c1551189af1b8",
+    "url": "/static/media/item-blue.8bcbdffe.jpg"
+  }
+]);
